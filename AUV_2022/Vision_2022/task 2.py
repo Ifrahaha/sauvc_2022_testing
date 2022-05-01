a820ae4587e10433aa7c7133f573946460e9236b
Zahid Hussain <zahidhussain909@gmail.com>
2022-05-02 05:29:47 +0530

    task 2 function division main

--- AUV_2022/Vision_2022/task 2.py
+++ AUV_2022/Vision_2022/task 2.py
@@ -17,283 +17,131 @@ bpts = deque(maxlen=64)
 
 
 vs1 = VideoStream(src=0).start()
-vs2 = VideoStream(src=1).start()
+vs2 = VideoStream(src=0).start()
 
-def shape_detect(c):
-		# initialize the shape name and approximate the contour
-	shape = "unidentified"
-	peri = cv2.arcLength(c, True)
-	approx = cv2.approxPolyDP(c, 0.04 * peri, True)
-	if len(approx)>=5:
-		shape = "circle"
-	return shape
+def shape_detect(frame):
+    shape = "unidentified"
+    resized = imutils.resize(frame, width=300)
+    ratio = frame.shape[0] / float(resized.shape[0])
+    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+        if len(approx)>=5:
+            shape = "circle"
+        try:
+            M = cv2.moments(c)
+            cX = int((M["m10"] / M["m00"]) * ratio)
+            cY = int((M["m01"] / M["m00"]) * ratio)
+            c = c.astype("float")
+            c *= ratio
+            c = c.astype("int")
+            cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
+            cv2.putText(frame, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (255, 255, 255), 2)
+            return frame
+        except ZeroDivisionError as e:
+                pass
+    return frame
 
+
+def detect_color(frame,lower,upper,deq):
+    frame = imutils.resize(frame, width=600)
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    (h, w) = frame.shape[:2] 
+    cv2.circle(frame, (w//2, h//2), 3, (255, 255, 255), -1) 
+    (h, w) = frame.shape[:2] 
+    mask = cv2.inRange(hsv, lower, upper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
+    if len(cnts) > 0:
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        if radius > 10:
+            cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+    deq.appendleft(center)
+    return deq
+    
+
+def grid(frame,deq):
+    (h, w) = frame.shape[:2] #h=y-axis, w=x-axis
+    if (deq[i][0]<(w/3) and deq[i][1]<(h/3)):
+        print("up left")
+
+    if (deq[i][0]>(w/3) and deq[i][0]<(2*w/3) and deq[i][1]<(h/3)):
+        print("up")
+
+    if (deq[i][0]>(2*w/3) and deq[i][1]<(h/3)):
+        print("up right")
+
+    if (deq[i][0]<(w/3) and deq[i][1]>(h/3) and deq[i][1]<(2*h/3)):
+        print("left")
+
+    if (deq[i][0]>(w/3) and deq[i][0]<(2*w/3) and deq[i][1]>(h/3) and deq[i][1]<(2*h/3)):
+        print("center")
+
+    if (deq[i][0]>(2*w/3) and deq[i][1]>(h/3) and deq[i][1]<(2*h/3)):
+        print("right")
+
+    if (deq[i][0]<(w/3) and deq[i][1]>(2*h/3)):
+        print("down left")
+
+    if (deq[i][0]>(w/3) and deq[i][0]<(2*w/3) and deq[i][1]>(2*h/3)):
+        print("down")
+
+    if (deq[i][0]>(2*w/3) and deq[i][1]>(2*h/3)):
+        print("down right")
 
 time.sleep(2.0)
 
-# keep looping
+
 while True:
-	# grab the current frame
-	frame = vs1.read()
+    color = (255, 0, 0)
+    thickness = 1
+    frame = vs1.read()
+    if frame is None:
+        break
+    pts=detect_color(frame,greenLower,greenUpper,pts)
+    for i in range(1, len(pts)):
+        if pts[i - 1] is None or pts[i] is None:
+            continue
+        thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
+        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+        grid(frame,pts)
+    while(len(pts)==0):
+        frame1 = vs2.read()
+        if frame1 is None:
+            break
+        bpts=detect_color(frame1,blueLower,blueUpper,bpts)
+        for i in range(1, len(bpts)):
+            if bpts[i - 1] is None or bpts[i] is None:
+                continue
+            thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
+            cv2.line(frame1, bpts[i - 1], bpts[i], (0, 0, 255), thickness)
+            counter=0
+            grid(frame1,bpts)
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
 
-	
-	if frame is None:
-		break
-
-	# resize the frame, blur it, and convert it to the HSV
-	# color space
-	frame = imutils.resize(frame, width=600)
-	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-
-
-	(h, w) = frame.shape[:2] #w:image-width and h:image-height
-	cv2.circle(frame, (w//2, h//2), 3, (255, 255, 255), -1) 
-	#cv2.circle(frame, (w//2, h//2), 113, (255, 255, 255), -1) 
-	color = (255, 0, 0)
-	thickness = 1
-	(h, w) = frame.shape[:2] #h=y-axis, w=x-axis
-	
-	# start_point =(0,0)
-	# end_point =((w/3),(h/3))
-	# cv2.rectangle(frame, start_point, end_point, color, thickness)
-
-
-	# start_point =((w/3),0)
-	# end_point =((2*w/3),(h/3))
-	# cv2.rectangle(frame, start_point, end_point, color, thickness)
-
-	# start_point =((2*w/3),0)
-	# end_point =((2*w/3),(h/3))
-	# cv2.rectangle(frame, start_point, end_point, color, thickness)
-
-
-	# construct a mask for the color "green", then perform
-	# a series of dilations and erosions to remove any small
-	# blobs left in the mask
-	mask = cv2.inRange(hsv, greenLower, greenUpper)
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
-
-    	# find contours in the mask and initialize the current
-	# (x, y) center of the ball
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
-	center = None
-
-	# only proceed if at least one contour was found
-	if len(cnts) > 0:
-		# find the largest contour in the mask, then use
-		# it to compute the minimum enclosing circle and
-		# centroid
-		c = max(cnts, key=cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-		# only proceed if the radius meets a minimum size
-		if radius > 10:
-			# draw the circle and centroid on the frame,
-			# then update the list of tracked points
-			cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
-			cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
-
-
-	# update the points queue
-	pts.appendleft(center)
-
-    	# loop over the set of tracked points
-	for i in range(1, len(pts)):
-		# if either of the tracked points are None, ignore
-		# them
-		if pts[i - 1] is None or pts[i] is None:
-			continue
-
-		# otherwise, compute the thickness of the line and
-		# draw the connecting lines
-		thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
-		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-		
-		(h, w) = frame.shape[:2] #h=y-axis, w=x-axis
-		cx=w/2
-		cy=h/2
-
-		#detecting green
-
-		if (pts[i][0]<(w/3) and pts[i][1]<(h/3)):
-			print("up left")
-
-		if (pts[i][0]>(w/3) and pts[i][0]<(2*w/3) and pts[i][1]<(h/3)):
-			print("up")
-
-		if (pts[i][0]>(2*w/3) and pts[i][1]<(h/3)):
-			print("up right")
-
-		if (pts[i][0]<(w/3) and pts[i][1]>(h/3) and pts[i][1]<(2*h/3)):
-			print("left")
-
-		if (pts[i][0]>(w/3) and pts[i][0]<(2*w/3) and pts[i][1]>(h/3) and pts[i][1]<(2*h/3)):
-			print("center")
-
-		if (pts[i][0]>(2*w/3) and pts[i][1]>(h/3) and pts[i][1]<(2*h/3)):
-			print("right")
-
-		if (pts[i][0]<(w/3) and pts[i][1]>(2*h/3)):
-			print("down left")
-
-		if (pts[i][0]>(w/3) and pts[i][0]<(2*w/3) and pts[i][1]>(2*h/3)):
-			print("down")
-
-		if (pts[i][0]>(2*w/3) and pts[i][1]>(2*h/3)):
-			print("down right")
-	print(pts)
-	while(len(pts)==0):
-		frame1 = vs2.read()
-		if frame1 is None:
-			break
-		frame1 = imutils.resize(frame1, width=600)
-		blurred = cv2.GaussianBlur(frame1, (11, 11), 0)
-		hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-		mask = cv2.inRange(hsv, blueLower, blueUpper)
-		mask = cv2.erode(mask, None, iterations=2)
-		mask = cv2.dilate(mask, None, iterations=2)
-
-		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-			cv2.CHAIN_APPROX_SIMPLE)
-		cnts = imutils.grab_contours(cnts)
-		center = None
-		if len(cnts) > 0:
-			c = max(cnts, key=cv2.contourArea)
-			((x, y), radius) = cv2.minEnclosingCircle(c)
-			M = cv2.moments(c)
-			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-			if radius > 10:
-				cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
-				cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
-		bpts.appendleft(center)
-
-		resized = imutils.resize(frame, width=300)
-		ratio = frame.shape[0] / float(resized.shape[0])
-		# convert the resized image to grayscale, blur it slightly,
-		# and threshold it
-		gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-		blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-		thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
-		# find contours in the thresholded image and initialize the
-		# shape detector
-		cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-			cv2.CHAIN_APPROX_SIMPLE)
-		cnts = imutils.grab_contours(cnts)
-
-		# loop over the contours
-		for c in cnts:
-			# compute the center of the contour, then detect the name of the
-			# shape using only the contour
-			try:
-				M = cv2.moments(c)
-				cX = int((M["m10"] / M["m00"]) * ratio)
-				cY = int((M["m01"] / M["m00"]) * ratio)
-				shape = shape_detect(c)
-				# multiply the contour (x, y)-coordinates by the resize ratio,
-				# then draw the contours and the name of the shape on the image
-				c = c.astype("float")
-				c *= ratio
-				c = c.astype("int")
-				cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
-				cv2.putText(frame, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-					0.5, (255, 255, 255), 2)
-				sd = shape_detect(c)
-
-				if (sd == "")
-			except ZeroDivisionError as e:
-				pass
-
-		for i in range(1, len(bpts)):
-
-		# if either of the tracked points are None, ignore
-		# them
-			if bpts[i - 1] is None or bpts[i] is None:
-				continue
-
-			# otherwise, compute the thickness of the line and
-			# draw the connecting lines
-			thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
-			cv2.line(frame1, bpts[i - 1], bpts[i], (0, 0, 255), thickness)
-			
-			(h, w) = frame1.shape[:2] #h=y-axis, w=x-axis
-			cx=w/2
-			cy=h/2
-
-			#detecting green
-			counter=0
-			if (bpts[i][0]<(w/3) and bpts[i][1]<(h/3)):
-				#print("up left")
-				print("forward")
-
-			if (bpts[i][0]>(w/3) and bpts[i][0]<(2*w/3) and bpts[i][1]<(h/3)):
-				#print("up")
-				print("forward")
-
-			if (bpts[i][0]>(2*w/3) and bpts[i][1]<(h/3)):
-				#print("up right")
-				print("forward")
-
-			if (bpts[i][0]<(w/3) and bpts[i][1]>(h/3) and bpts[i][1]<(2*h/3)):
-				count = count + 1
-				print("left")
-
-			if (bpts[i][0]>(w/3) and bpts[i][0]<(2*w/3) and bpts[i][1]>(h/3) and bpts[i][1]<(2*h/3)):
-				count = count + 1
-				print("center")
-
-			if (bpts[i][0]>(2*w/3) and bpts[i][1]>(h/3) and bpts[i][1]<(2*h/3)):
-				count = count + 1
-				print("right")
-
-			if (bpts[i][0]<(w/3) and bpts[i][1]>(2*h/3)):
-				#print("down left")
-				print("backward")
-
-			if (bpts[i][0]>(w/3) and bpts[i][0]<(2*w/3) and bpts[i][1]>(2*h/3)):
-				#print("down")
-				print("backward")
-
-			if (bpts[i][0]>(2*w/3) and bpts[i][1]>(2*h/3)):
-				#print("down right")
-				print("backward")
-
-
-		
-
-	# show the frame to our screen
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
-	#print(pts)
-	
-	# if the 'q' key is pressed, stop the loop
-	if key == ord("q"):
-		break
-
-# if we are not using a video file, stop the camera video stream
 if not args.get("video", False):
-	vs.stop()
-
-# otherwise, release the camera
+    vs.stop()
+    vs1.stop()
 else:
-	vs.release()
-
-# close all windows
+    vs.release()
+    vs1.release()
 cv2.destroyAllWindows()
-
-def shape_detect(c):
-		# initialize the shape name and approximate the contour
-	shape = "unidentified"
-	peri = cv2.arcLength(c, True)
-	approx = cv2.approxPolyDP(c, 0.04 * peri, True)
-	if len(approx)>=5:
-		shape = "circle"
-	return shape
-
