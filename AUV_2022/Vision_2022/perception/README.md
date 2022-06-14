7f7e3b3889a7ded0a0d8860dfc44771a3da873f3
Zahid Hussain <zahidhussain909@gmail.com>
2022-06-15 00:36:30 +0530

    task1

---
+++ AUV_2022/Vision_2022/perception/README.md
@@ -0,0 +1,16 @@
+#CODE BY ZAHID AND IFRAH for task 1 
+###Gate detection without red flare avoidance
+
+
+## Perception Code Overview
+
+conda create -n urobotics python=3.7
+conda activate urobotics
+pip3 install -r requirements.txt
+pip3 install -e perception/
+
+cd perception 
+python setup.py build_ext --inplace
+cythonize file_to_cythonize.pyx
+
+
--- AUV_2022/Vision_2022/task 2.py
+++ AUV_2022/Vision_2022/task 2.py
@@ -50,6 +50,59 @@ def shape_detect(frame):
     return frame
 
 
+
+def shape_detect_color(frame,lower,upper,deq):
+    shape = "unidentified"
+    resized = imutils.resize(frame, width=600)
+    ratio = frame.shape[0] / float(resized.shape[0])
+    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+        if len(approx)>=5:
+            shape = "circle"
+            blurred = cv2.GaussianBlur(resized, (11, 11), 0)
+            hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+            (h, w) = frame.shape[:2] 
+            cv2.circle(frame, (w//2, h//2), 3, (255, 255, 255), -1) 
+            (h, w) = frame.shape[:2] 
+            mask = cv2.inRange(hsv, lower, upper)
+            mask = cv2.erode(mask, None, iterations=2)
+            mask = cv2.dilate(mask, None, iterations=2)
+            cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                cv2.CHAIN_APPROX_SIMPLE)
+            cnts = imutils.grab_contours(cnts)
+            center = None
+            if len(cnts) > 0:
+                c = max(cnts, key=cv2.contourArea)
+                ((x, y), radius) = cv2.minEnclosingCircle(c)
+                M = cv2.moments(c)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+                if radius > 10:
+                    cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+                    cv2.circle(frame, center, 5, (0, 0, 255), -1)
+            deq.appendleft(center)
+        try:
+            M = cv2.moments(c)
+            cX = int((M["m10"] / M["m00"]) * ratio)
+            cY = int((M["m01"] / M["m00"]) * ratio)
+            c = c.astype("float")
+            c *= ratio
+            c = c.astype("int")
+            cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
+            cv2.putText(frame, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (255, 255, 255), 2)
+            return frame
+        except ZeroDivisionError as e:
+                pass
+    return deq
+
+
 def detect_color(frame,lower,upper,deq):
     frame = imutils.resize(frame, width=600)
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
@@ -114,7 +167,9 @@ while True:
     frame = vs1.read()
     if frame is None:
         break
-    pts=detect_color(frame,greenLower,greenUpper,pts)
+    
+    #pts=detect_color(frame,greenLower,greenUpper,pts)
+    pts =shape_detect_color(frame,greenLower,greenUpper,pts)
     for i in range(1, len(pts)):
         if pts[i - 1] is None or pts[i] is None:
             continue
