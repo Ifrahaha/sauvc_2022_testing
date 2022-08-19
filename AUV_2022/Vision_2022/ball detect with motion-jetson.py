79ddd531d66645285719991de5473c9c2eef0e4a
Zahid Hussain <zahidhussain909@gmail.com>
2022-08-19 14:18:20 +0530

    arming the test

--- AUV_2022/Vision_2022/ball detect with motion-jetson.py
+++ AUV_2022/Vision_2022/ball detect with motion-jetson.py
@@ -8,6 +8,29 @@ import imutils
 import time
 import movement-jetson
 
+from pymavlink import mavutil
+
+# Create the connection
+master = mavutil.mavlink_connection("/dev/ttyACM0", baud=57600)
+# Wait a heartbeat before sending commands
+master.wait_heartbeat()
+
+# https://mavlink.io/en/messages/common.html#MAV_CMD_COMPONENT_ARM_DISARM
+
+# Arm
+# master.arducopter_arm() or:
+master.mav.command_long_send(
+    master.target_system,
+    master.target_component,
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+    0,
+    1, 0, 0, 0, 0, 0, 0)
+
+# # wait until arming confirmed (can manually check with master.motors_armed())
+print("Waiting for the vehicle to arm")
+master.motors_armed_wait()
+print('Armed!')
+
 m=Movement.movement()
 
 ap = argparse.ArgumentParser()
