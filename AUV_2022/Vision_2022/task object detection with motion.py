6fcb6964ea385f0ddbabfe4f9211dff7073a7129
Zahid Hussain <zahidhussain909@gmail.com>
2022-08-27 21:19:22 +0530

    bounding box center

--- AUV_2022/Vision_2022/task object detection with motion.py
+++ AUV_2022/Vision_2022/task object detection with motion.py
@@ -187,28 +187,29 @@ def detect(save_img=False):
             print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
 
 #############################################################################
-            x1 = int(xyxy[0].item())
-            y1 = int(xyxy[1].item())
-            x2 = int(xyxy[2].item())
-            y2 = int(xyxy[3].item())
-            x_cen = x2 - x1
-            y_cen = y2 - y1
-            center = (x_cen, y_cen)
-            pts.appendleft(center)
+            for *xyxy, conf, cls in reversed(det):
+                x1 = int(xyxy[0].item())
+                y1 = int(xyxy[1].item())
+                x2 = int(xyxy[2].item())
+                y2 = int(xyxy[3].item())
+                x_cen = x2 - x1
+                y_cen = y2 - y1
+                center = (x_cen, y_cen)
+                pts.appendleft(center)
 
-            thickness = 1
+                thickness = 1
 
-            for i in range(1, len(pts)):
-                if pts[i - 1] is None or pts[i] is None:
-                    continue
-                thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
-                cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-                grid(frame,pts)
+                for i in range(1, len(pts)):
+                    if pts[i - 1] is None or pts[i] is None:
+                        continue
+                    thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
+                    cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+                    grid(frame,pts)
 
-            # Stream results
-            if view_img:
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(1)  # 1 millisecond
+                # Stream results
+                if view_img:
+                    cv2.imshow(str(p), im0)
+                    cv2.waitKey(1)  # 1 millisecond
 
 ########################################################################
 
